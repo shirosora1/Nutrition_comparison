f4957bfe925dac649bfa545e4a101e13b7c549e2
shirosora1 <13071798+shirosora1@users.noreply.github.com>
2024-11-08 16:25:05 +0000

    Trying to add nutritional details as part of the food object

--- src/Main.java
+++ src/Main.java
@@ -9,19 +9,15 @@ public class Main {
         System.out.println("Enter First product details - ");
         Food firstProduct = ProductInputHandler.getFoodInput(scanner);
 
-
     // Second product
         System.out.println("Enter Second product details - ");
         Food secondProduct = ProductInputHandler.getFoodInput(scanner);
-
 
         System.out.println();
         System.out.println("First product: ");
         System.out.println(firstProduct.getDetails());
 
-
         System.out.println("Second product: ");
         System.out.println(secondProduct.getDetails());
-
     }
 }
